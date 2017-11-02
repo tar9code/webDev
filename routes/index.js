49ed6149fd69d08ebe0a291d991249d07d1a90a1
tar9code <tarstarck@gmail.com>
2017-11-02 11:29:36 -0700

    update for phpHerokuWebsite

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,28 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+/*
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+*/
 router.get('/doit', function(req, res, next) {
     res.render('doit', { title: 'Doit' });
 });
 
 module.exports = router;
 
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+var querystring = require('querystring'); //for use in GET Query string of form URI/path?name=value
+
+router.use(bodyParser.json()); // for parsing application/json
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencode
+//#########################################
+
+//now processing post
+router.post('/readNameAndRespond', function(req, res, next) {
+    //expecting data variable called name --retrieve value using body-parser
+    var value_name = req.body.name;  //retrieve the data associated with name
+    res.send("hello " + value_name);
+});
