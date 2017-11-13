6cc3ae0fd5c94270151bdff052573d757e39e213
tar9code <tarstarck@gmail.com>
2017-11-13 08:50:49 -0800

    monday 11/13 -- mongoDB database 1

---
+++ controllers/database.js
@@ -0,0 +1,53 @@
+var mongodb = require('mongodb');
+var mongoDBURI = process.env.MONGODB_URI || 'mongodb://heroku_1n3hp41c:database925webDev@ds153015.mlab.com:53015/heroku_1n3hp41c';
+
+/** getAllRoutes controller logic that current does model logic too -connects to Mongo database and
+ * queries the Routes collection to retrieve all the routes and build the output usig the
+ * ejs template mongodb.ejs found in views directory
+ * @param request
+ * @param response
+ *
+ */
+module.exports.getAllORDERS =  function (request, response) {
+
+    mongodb.MongoClient.connect(mongoDBURI, function(err, db) {
+        if(err) throw err;
+
+        //get collection of routes
+        var Routes = db.collection('ORDERS');
+
+
+        //FIRST showing you one way of making request for ALL routes and cycle through with a forEach loop on returned Cursor
+        //   this request and loop  is to display content in the  console log
+        var c = Routes.find({});
+
+        c.forEach(
+            function(myDoc) {
+                console.log( "name: " + myDoc.name );  //just  loging the output to the console
+            }
+        );
+
+
+        //SECOND -show another way to make request for ALL Routes  and simply collect the  documents as an
+        //   array called docs that you  forward to the  getAllRoutes.ejs view for use there
+        Routes.find().toArray(function (err, docs) {
+            if(err) throw err;
+
+            response.render('getAllRoutes', {results: docs});
+
+        });
+
+
+        //Showing in comments here some alternative read (find) requests
+        //this gets Routes where frequency>=10 and sorts by name
+        // Routes.find({ "frequency": { "$gte": 10 } }).sort({ name: 1 }).toArray(function (err, docs) {
+        // this sorts all the Routes by name
+        //  Routes.find().sort({ name: 1 }).toArray(fu namenction (err, docs) {
+
+
+        //close connection when your app is terminating.
+        db.close(function (err) {
+            if(err) throw err;
+        });
+    });//end of connect
+};//end function
